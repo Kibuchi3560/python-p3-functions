c3d04222b7147228ca3255c7a3f60b95bab375ff
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-11-27 14:42:27 +0300

    Finished lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,51 @@
-#!/usr/bin/env python3
+# def greet_programmer():
+#     print ("Hello, programmer!")
+
+# pass
+
+# greet_programmer();
+
+# def greet(name ):
+#    print(f"Hello, ${name}!")
+#    pass
+
+# greet("Kibuchi");
+
+# def greet_with_default(name ="programmer"):
+#     print(f"Hello, ${name}!")
+#     pass
+
+
+# greet_with_default();
+
+# def add(num1 = 2, num2 = 3):
+#   return num1 + num2
+
+# add();
+
+# def halve(num2 = 2):
+#    return num2 * 0.5
+
+# halve();    
+
+
+
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
